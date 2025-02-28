87702381236959fca0a05fdf1511ffd565ec6550
lupamo <arnoldlupamo@gmail.com>
2025-02-28 21:54:53 +0300

    admin add

--- frontend/src/components/AdminSidebar.jsx
+++ frontend/src/components/AdminSidebar.jsx
@@ -13,7 +13,7 @@ const AdminSidebar = ({ setActiveComponent, activeComponent }) => {
 					<img className="w-5 h-5" src={assets.list} alt="add" />
 					<p className="hidden md:block">List Items</p>
 				</button>
-				<button onClick={() => setActiveComponent('orders')} className={`flex items-center gap-3 border border-gray-400 border-r-0 px-3 py-2 rounded-l ${activeComponent === 'orders' ? 'bg-blue-100' : ''}`}>
+				<button onClick={() => setActiveComponent('placed-orders')} className={`flex items-center gap-3 border border-gray-400 border-r-0 px-3 py-2 rounded-l ${activeComponent === 'orders' ? 'bg-blue-100' : ''}`}>
 					<img className="w-5 h-5" src={assets.orders} alt="add" />
 					<p className="hidden md:block">Orders</p>
 				</button>
